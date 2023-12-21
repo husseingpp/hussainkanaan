5683f0db77fa13479bcf3649b4c27a7958c57d83
groot0y <95100870+groot0y@users.noreply.github.com>
2023-12-21 19:31:34 +0200

    django work
    adding the other html pages to the main page

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def say_hello(request):
+def main_view(request):
     return render(request,'main.html')
+
+def elon_page(request):
+    return render(request,'elon-musk.html')
+
+def coffe_page(request):
+    return render(request,'coffe.html')
+
+def cal_page(request):
+    return render(request,'calculator.html')
